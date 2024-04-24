a5cd72438a4c160fc9e624ac8fa7c6721f653de2
AI-Ashik <arifulislamashik07@gmail.com>
2024-04-24 13:04:18 +0600

    created component(hero component) and created starting part of hero

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,20 @@
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hello, I am A.I Ashik
+          </h1>
+          <p className="text-[#ABD7BE] text-lg lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+            repudiandae iusto ex quas est voluptatem, et tenetur facilis nisi
+            inventore commodi.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
