adc2d86adbaeb5c70de7ed66c155c424f3575240
AI-Ashik <arifulislamashik07@gmail.com>
2024-04-28 16:01:23 +0600

    little code customization and added type animation with the help for typeanimation npm package

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,24 +1,47 @@
+"use client";
 import Image from "next/image";
+
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, I am A.I Ashik
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-red-600">
+              Hello, I am{" "}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                "Ashik",
+                1000,
+                "MERN Stack Developer",
+                1000,
+                "Wordpress Developer",
+                1000,
+                "SEO Specialist",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
-          <p className="text-[#ABD7BE] text-lg lg:text-xl mb-6">
+          <p className="text-[#ABD7BE] text-base sm:text-lg lg:text-xl mb-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
             repudiandae iusto ex quas est voluptatem, et tenetur facilis nisi
             inventore commodi.
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-red-500 hover:bg-slate-200 text-white">
               Hire me{" "}
             </button>
-            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
-              Donwload CV
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-red-500 hover:bg-slate-800 text-white  mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Donwload CV
+              </span>
             </button>
           </div>
         </div>
