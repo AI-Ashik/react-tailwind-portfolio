50e2587dfb58cf467c1a20f92ed6d98567a1bc75
AI-Ashik <arifulislamashik07@gmail.com>
2024-05-01 15:25:53 +0600

    created button for responsive menu still remaining main functionality and design

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,4 +1,7 @@
+"use client";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import { useState } from "react";
 import NavLink from "./NavLink";
 
 const navLinks = [
@@ -17,14 +20,29 @@ const navLinks = [
 ];
 
 const Navbar = () => {
+  const [navBarOpen, setNavBarOpen] = useState(false);
   return (
-    <nav>
-      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-        <Link href={"/"} className="text-5xl text-white font-semibold">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212 ] bg-opacity-90">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
           Logo
         </Link>
-        <div className="menu block md:w-auto" id="navbar">
-          <ul>
+        <div className="mobile-menu block md:hidden ">
+          {navBarOpen ? (
+            <button className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+              <Bars3Icon className="h-5 w-5" />
+            </button>
+          ) : (
+            <button className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+              <XMarkIcon className="h-5 w-5" />
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
               <li key={index}>
                 <NavLink href={link.path} title={link.title} />
