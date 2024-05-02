93d866c66993c69d0577ccd7f64c39d5ba409bab
AI-Ashik <arifulislamashik07@gmail.com>
2024-05-02 20:00:57 +0600

    little Design Optimization for md screens

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
       <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
-          className="text-2xl md:text-5xl text-white font-semibold"
+          className="text-2xl md:text-4xl text-white font-semibold"
         >
           Logo
         </Link>
