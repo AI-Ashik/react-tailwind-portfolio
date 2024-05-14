8f7f834de81c114553d72a051972e5ff7d603dc1
AI-Ashik <arifulislamashik07@gmail.com>
2024-05-14 16:07:12 +0600

    designed the email section and setup the api

---
+++ app/components/EmailSection.jsx
@@ -0,0 +1,84 @@
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
+import Link from "next/link";
+import FacebookIcon from "/public/icons/facebook.png";
+
+const EmailSection = () => {
+  return (
+    <section className="grid md:grid-cols-2 my-12 gap-4 py-24 ">
+      <div>
+        <h5 className="text-xl font-bold text-white my-2 ">Let's connect</h5>
+        <p className="text-[#ADB7BE] mb-4 max-w-md">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis
+          nisi molestias sint expedita ipsum quas tenetur odit. Minima voluptas
+          asperiores quidem reiciendis sequi.
+        </p>
+        <div
+          className="socials flex flex-row
+         gap-2"
+        >
+          <Link href="github.com">
+            <Image
+              src={FacebookIcon}
+              alt="github icon"
+              height={32}
+              width={32}
+            />
+          </Link>
+        </div>
+      </div>
+      <div>
+        <form className="flex flex-col gap-4" action="">
+          <div>
+            <label htmlFor="email" className="text-white text-sm font-medium">
+              Your E-Mail :{" "}
+            </label>
+            <input
+              type="email"
+              id="email"
+              required
+              placeholder="example@example.com"
+              className="bg-[#18191e] border border-[#33353f] text-gray-100 placeholder-[#9CA2A9] text-sm rounded-lg w-full p-2.5"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="subject" className="text-white text-sm font-medium">
+              Your Subject :{" "}
+            </label>
+            <input
+              type="text"
+              id="subject"
+              required
+              placeholder="Your Subject"
+              className="bg-[#18191e] border border-[#33353f] text-gray-100 placeholder-[#9CA2A9] text-sm rounded-lg w-full p-2.5"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="subject" className="text-white text-sm font-medium">
+              Your Message :{" "}
+            </label>
+            <textarea
+              name="message"
+              id="subject"
+              required
+              placeholder="Your Message"
+              rows={8}
+              cols={8}
+              className="bg-[#18191e] border border-[#33353f] text-gray-100 placeholder-[#9CA2A9] text-sm rounded-lg w-full p-2.5"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default EmailSection;
